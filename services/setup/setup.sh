c8107004b7eb8d8ee77f48919a212695e6b87d48
Stefan Nožinić <stefan@lugons.org>
2020-06-05 17:55:11 +0200

    Integrate order_handling service

--- services/setup/setup.sh
+++ services/setup/setup.sh
@@ -4,5 +4,6 @@ export PGPASSWORD="admin"
 
 sleep 2
 
-psql \
-    --host database --user postgres --no-password -c 'create database "XMLProject"'  -c 'grant ALL on database "XMLProject" to "postgres"'
+psql --host database --user postgres --no-password -c 'create database "message_db"'  -c 'grant ALL on database "message_db" to "postgres"'
+psql --host database --user postgres --no-password -c 'create database "order_db"'  -c 'grant ALL on database "order_db" to "postgres"'
+
