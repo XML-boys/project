7d623c7d94ce8ea7cfd56f99c68a1a116ce289d8
Stefan Nožinić <stefan@lugons.org>
2020-06-05 18:14:50 +0200

    Integrate user service

--- services/setup/setup.sh
+++ services/setup/setup.sh
@@ -6,4 +6,6 @@ sleep 2
 
 psql --host database --user postgres --no-password -c 'create database "message_db"'  -c 'grant ALL on database "message_db" to "postgres"'
 psql --host database --user postgres --no-password -c 'create database "order_db"'  -c 'grant ALL on database "order_db" to "postgres"'
+psql --host database --user postgres --no-password -c 'create database "user_db"'  -c 'grant ALL on database "user_db" to "postgres"'
+
 
