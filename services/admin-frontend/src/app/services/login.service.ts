5e78db7271bf23f9458bc0ffb088c73859827085
BorislavGajic <57411821+BorislavGajic@users.noreply.github.com>
2020-06-28 17:20:10 +0200

    prepravke security-a

--- services/admin-frontend/src/app/services/login.service.ts
+++ services/admin-frontend/src/app/services/login.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   login(user): Observable<Response> {
     return this.http.post<Response>('http://localhost:6969/auth', user);
   }
 
-  getToken(): string{
+  getToken(): string {
+    console.log(localStorage.getItem('jwt'));
     return localStorage.getItem('jwt');
+  }
+
+  logout() {
+    localStorage.removeItem('jwt');
+    this.router.navigate(['/']);
   }
 }
